9e2d9bd1d16ec72b855629270dc9a9af3798918e
John Skovbekk <jmcjack@gmail.com>
2023-08-17 17:06:09 +0000

    Improve visualization functions

--- src/visualize.jl
+++ src/visualize.jl
@@ -1,35 +1,41 @@
 # Tools for plotting
 
 function create_shape(state)
-    shape = Plots.Shape([state[1][1], state[1][2], state[1][2], state[1][1]], 
-                        [state[2][1], state[2][2], state[2][2], state[2][1]])
+    shape = Plots.Shape([state[1,1], state[1,2], state[1,2], state[1,1]], 
+                        [state[2,1], state[2,1], state[2,2], state[2,2]])
     return shape
 end
 
-function plot_with_alpha(state, alpha)
-    plt = plot()
-    plot_with_alpha!(plt, state, alpha)
+function plot_with_alpha(states, alpha)
+    plt = plot(aspect_ratio=1)
+    plot_with_alpha!(plt, states, alpha)
     return plt
 end
 
-function plot_with_alpha!(plt, state, alpha)
-    plot!(plt, create_shape(state), fillcolor=:blue, fillalpha=alpha, alpha=0.1, label="")
+function plot_with_alpha!(plt, states, alpha)
+        plot!(plt, create_shape.(states), fillcolor=:blue, fillalpha=alpha, alpha=0.1, label="")
 end
 
-function plot_with_classifications(state, classifications)
-    plt = plot()
-    plot_with_classifications!(plt, state, classifications)
+function plot_with_classifications(states, classifications)
+    plt = plot(aspect_ratio=1)
+    plot_with_classifications!(plt, states, classifications)
     return plt
 end
 
-function plot_with_classifications!(plt, state, classifications)
-    color = :yellow
-    if classifications == 1
-        color = :green
-    elseif classifications == 2
-        color = :red
+function plot_with_classifications!(plt, states, classifications)
+    # make an array with just yellow
+    colors = fill(:yellow, length(classifications))
+    # change the colors of the states that are classified
+    colors[classifications .== 1] .= :green
+    colors[classifications .== 2] .= :red
+    plot!(plt, create_shape.(states), fillcolor=colors, fillalpha=1.0, alpha=0.1, label="")
+end
+
+function plot_state_labels!(plt, states)
+    for (i, state) in enumerate(states)
+        state_mean = calculate_state_mean(state)
+        annotate!(plt, state_mean[1], state_mean[2], text("$i", :black, :center, 11,))
     end
-    plot!(plt, create_shape(state), fillcolor=color, fillalpha=1.0, alpha=0.1, label="")
 end
 
 function save_figure_files(plt, filename)
